63cb40e0c65d8ec9d3538da86c9fb82f778d7fce
lucile berger <regrebl9@gmail.com>
2023-10-04 14:44:28 +0200

    Lucile Berger TP1 partie 2

--- TP0_BERGER/TP1_convertisseur_lucile_berger/src/tp1_convertisseur_lucile_berger/TP1_convertisseur_lucile_berger.java
+++ TP0_BERGER/TP1_convertisseur_lucile_berger/src/tp1_convertisseur_lucile_berger/TP1_convertisseur_lucile_berger.java
@@ -16,100 +16,92 @@ public class TP1_convertisseur_lucile_berger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
+        Scanner sc = new Scanner(System.in);
+
         double intro;
         System.out.print("Bonjour, saisissez une valeur : ");
         intro = sc.nextInt();
-        
-        double conv;
-        System.out.print("Saisissez la conversion que vous souhaiter effectuer :\n 1) De Celcius vers Kelvin\n 2) De Kelvin vers Celcius\n 6) De Farenheit vers Kelvin ");
-        conv = sc.nextInt();
 
-        double Tc;
-        System.out.print("Entrez la température en degré Celcius : ");
-        Tc = sc.nextInt();
+        int choix = 0;
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :\n 1) De Celcius vers Kelvin\n 2) De Kelvin vers Celcius\n 3) De Farenheit vers Celcius \n 4) De Kelvin vers Farenheit \n 5) De Farenheit vers Kelvin");
+        choix = sc.nextInt();
+
+        double result;
+
+        if (choix == 1) {
+            result = CelciusVersKelvin(intro);
+            System.out.println("Celcius vers Kelvin : " + result + "K");
+        }
+
         
-        double Tf;
-        System.out.print("Entrez la température en Farenheit :");
-        Tf = sc.nextInt();
-        
-        
-        
-        
-        
-        
-        double Tk;
-        Tk = Tc + 273.15;
-        System.out.println("La température en Kelvin : " +Tk+ "K");
-  
-        double ck;
-        ck = CelciusVersKelvin(Tc);
-        System.out.println("Celvin vers Kelvin : " +ck+ "K");
-        
-        double kc;
-        kc = KelvinVersCelcius(Tk);
-        System.out.println("Kelvin Vers Celcius : " +kc+ "C");
-        
-        double fc;
-        fc = FarenheitVersCelcius(Tf);
-        System.out.println("Farenheit vers Celcius : " +fc+ "C");
-        
-        double kf;
-        kf = KelvinVersFarenheit(Tk);
-        System.out.println("Kelvin vers Farenheit : " +kc+ "F");
-        
-        double fk;
-        kf = FarenheitVersKelvin(Tf);
-        System.out.println("Farenheit vers Kelvin  : " +kf+ "K");
+
+        if (choix == 2) {
+            result = KelvinVersCelcius(intro);
+            System.out.println("Kelvin Vers Celcius : " + result + "C");
+        }
+
+        if (choix == 3) {
+            result = FarenheitVersCelcius(intro);
+            System.out.println("Farenheit vers Celcius : " + result + "C");
+        }
+            
+        if (choix == 4){
+            result = KelvinVersFarenheit(intro);
+            System.out.println("Kelvin vers Farenheit : " + result + "F");
+        }
+            
+        if (choix == 5){
+            result = FarenheitVersKelvin(intro);
+            System.out.println("Farenheit vers Kelvin  : " + result + "K");
+        }
     }
-    
-    public static double CelciusVersKelvin (double Tc) {
+
+    public static double CelciusVersKelvin(double Tc) {
 
         double Tk;
         Tk = Tc + 273.15;
         return Tk;
-       
-        }
-    
-    public static double KelvinVersCelcius (double Tk) {
+
+    }
+
+    public static double KelvinVersCelcius(double Tk) {
 
         double Tc;
         Tc = Tk - 273.15;
         return Tc;
-       
-        }
-    
+
+    }
+
     public static double FarenheitVersCelcius(double Tf) {
 
         double Tc;
         Tc = (Tf - 32) / 1.8;
         return Tc;
-       
-        }
+
+    }
 
     public static double CelciusVersFarenheit(double Tc) {
 
         double Tf;
-        Tf = (Tc * 1.8) + 32;  
+        Tf = (Tc * 1.8) + 32;
         return Tf;
-       
-        }
-        
+
+    }
+
     public static double KelvinVersFarenheit(double Tk) {
 
         double Tf;
         Tf = (Tk - 273.15) * 1.8 + 32;
         return Tf;
-       
-        }
-    
+
+    }
+
     public static double FarenheitVersKelvin(double Tf) {
 
         double Tk;
-        Tk = (Tf - 32) * 1.8 + 273.15 ;
+        Tk = (Tf - 32) * 1.8 + 273.15;
         return Tk;
-       
-        }
-    
+
+    }
+
 }
