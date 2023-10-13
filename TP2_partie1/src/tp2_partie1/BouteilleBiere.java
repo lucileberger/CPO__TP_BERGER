073c2e53d5b529e4c7c288cd4300956c40fb2a94
lucile berger <regrebl9@gmail.com>
2023-10-13 12:40:40 +0200

    Lucile Berger TP2 partie 1 bis, bouteille biere

--- TP2_partie1/src/tp2_partie1/BouteilleBiere.java
+++ TP2_partie1/src/tp2_partie1/BouteilleBiere.java
@@ -9,5 +9,35 @@ package tp2_partie1;
  * @author ocean
  */
 public class BouteilleBiere {
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+
+public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+}
+
     
+public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
+   nom = unNom;
+   degreAlcool = unDegre;
+   brasserie = uneBrasserie;
+   ouverte = false;
+}
+   
+public void Décapsuler() { 
+        if (ouverte == false) {
+            ouverte = true;
+        }
+    } 
+    
+    @Override
+    public String toString() {
+   String chaine_a_retourner;
+   chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
 }
