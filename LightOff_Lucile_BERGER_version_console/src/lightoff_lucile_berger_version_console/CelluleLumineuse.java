15efed6b658dea2d1bad5119f37c3b49f64386b5
lucile berger <regrebl9@gmail.com>
2023-10-25 12:28:32 +0200

    Lucile Berger mini projet, cellule lumineuse

---
+++ LightOff_Lucile_BERGER_version_console/src/lightoff_lucile_berger_version_console/CelluleLumineuse.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_lucile_berger_version_console;
+
+/**
+ *
+ * @author ocean
+ */
+public class CelluleLumineuse {
+    private boolean etat; 
+
+    /**
+     * Initialise la cellule en état "éteint".
+     */
+    public CelluleLumineuse() {
+        this.etat = false; 
+    }
+
+    /**
+     * Méthode pour activer/inverse la cellule.
+     */
+    public void activerCellule() {
+        this.etat = !this.etat;    // ! oppérateur de négation logique 
+    }
+
+    /**
+     * Passe la cellule dans l'état "éteint".
+     */
+    public void eteindreCellule() {
+        this.etat = false;
+    }
+
+    /**
+     * Méthode pour vérifier si la cellule est éteinte.
+     * @return true si la ceullue est est éteinte, sinon false si la ceullule est allumé.
+     */
+    public boolean estEteint() {
+        return !this.etat;
+    }
+
+    /**
+     * Méthode pour vérifier l'état de la cellule (allumé ou éteinte). 
+     * @return true si elle est allumée ou false si elle est éteinte.
+     */
+    public boolean getEtat() {
+        return this.etat;
+    }
+
+    /**
+     * Affiche l'état de la cellule de manière lisible.
+     * @return Si la cellule est allumé, elle affiche X, et si elle est éteinte elle affiche O.
+     */
+    @Override
+    public String toString() {
+        if (this.etat) {
+            return "X"; 
+        } else {
+            return "O"; 
+        }
+    }
+}
+
+
