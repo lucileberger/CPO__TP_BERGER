9676c7cb3c8cb61b594256165082afff96896019
lucile berger <regrebl9@gmail.com>
2023-10-27 20:36:23 +0200

    Lucile Berger mini projet, Réalisation d’une version initiale, fin

--- LightOff_Lucile_BERGER_version_console/src/lightoff_lucile_berger_version_console/LightOff_Lucile_BERGER_version_console.java
+++ LightOff_Lucile_BERGER_version_console/src/lightoff_lucile_berger_version_console/LightOff_Lucile_BERGER_version_console.java
@@ -5,30 +5,15 @@ package lightoff_lucile_berger_version_console;
 
 /**
  *
- * @author ocean
+ * @author lucile
  */
 public class LightOff_Lucile_BERGER_version_console {
-
-
-    public static void main(String[] args) {
-    
-       
-        CelluleLumineuse cellule1 = new CelluleLumineuse();
-        CelluleLumineuse cellule2 = new CelluleLumineuse();
-
-        
-        System.out.println("Etat initial de cellule1 : " + cellule1); 
-        
-        cellule1.activerCellule();
-        System.out.println("Nouvel etat de cellule1 : " + cellule1); 
-        
-        boolean etatCellule1 = cellule1.getEtat();
-        System.out.println("Etat de cellule1 : " + etatCellule1);
-        
      
-        
-    }
+    public static void main(String[] args) {
+        Partie partie = new Partie();
+        partie.initialiserPartie(); 
+        partie.lancerPartie(); 
+
+   
 }
-
-
-    
+}
