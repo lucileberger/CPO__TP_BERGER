2227bc59076a2d4f05c33e55f9ba7d7280d108b1
lucile berger <regrebl9@gmail.com>
2023-11-12 21:02:45 +0100

    Lucile Berger mini projet, partie

--- LightOff_Lucile_BERGER_version_console/src/lightoff_lucile_berger_version_console/Partie.java
+++ LightOff_Lucile_BERGER_version_console/src/lightoff_lucile_berger_version_console/Partie.java
@@ -7,21 +7,28 @@ import java.util.Scanner;
 
 /**
  *
- * @author lucile 
+ * @author Lucile Berger
  */
 
-   /*
+/*
  * Partie du jeu LightOff
  */
 public class Partie {
     GrilledeCellules grille;
     int nbCoups;
 
+    /**
+    * Initialise une nouvelle partie du jeu LightOff avec une grille de cellules de taille 7x7.
+    * Le nombre de coups joués est initialisé à zéro.
+     */
     public Partie() {
         grille = new GrilledeCellules(7, 7);
         nbCoups = 0;
     }
 
+    /**
+     * Initialise la partie en mélangeant aléatoirement les cellules de la grille.
+     */
     public void initialiserPartie() {
         grille.melangerMatriceAleatoirement(10); 
     }
@@ -32,6 +39,11 @@ public class Partie {
         partie.lancerPartie();
     }
 
+    /**
+    * Cette méthode permet de lancer une partie du jeu LightOff en interaction avec l'utilisateur.
+    * L'utilisateur peut activer une ligne, une colonne, une diagonale montante ou une diagonale descendante pour éteindre les cellules de la grille.
+    * La partie continue jusqu'à ce que toutes les cellules de la grille soient éteintes.
+     */
     public void lancerPartie() {
     Scanner scanner = new Scanner(System.in);
 
@@ -41,7 +53,7 @@ public class Partie {
 
     while (!grille.estTouteEteinte()) {
         System.out.println("Coup " + (nbCoups + 1));
-        System.out.println("Entrez 1 pour activer une ligne, 2 pour activer une colonne, 3 pour activer une diagonale montante, 4 pour activer une diagonale descendante : ");
+        System.out.println("Entrez 1 pour activer une ligne,\n 2 pour activer une colonne,\n 3 pour activer une diagonale montante,\n 4 pour activer une diagonale descendante : ");
         int choix = scanner.nextInt();
 
         switch (choix) {
